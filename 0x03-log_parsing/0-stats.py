3968556a65e3024571b075b28e0ec868ffb71839
Mariam <kelanimaryam1234@gmail.com>
2024-07-19 03:45:51 +0000

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,36 +1,46 @@
 #!/usr/bin/python3
-"""A script that that reads stdin line by line and computes metrics"""
+"""
+Log parsing
+"""
 
 import sys
 import re
-
+import signal
 
 def output(log: dict) -> None:
     """
-    helper function to display stats
+    Helper function to display stats
     """
     print("File size: {}".format(log["file_size"]))
     for code in sorted(log["code_frequency"]):
         if log["code_frequency"][code]:
             print("{}: {}".format(code, log["code_frequency"][code]))
 
+def signal_handler(sig, frame):
+    """
+    Signal handler to print stats on keyboard interruption
+    """
+    output(log)
+    sys.exit(0)
 
 if __name__ == "__main__":
     regex = re.compile(
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)'  # nopep8
+    )
 
     line_count = 0
     log = {}
     log["file_size"] = 0
-    log["code_frequency"] = {
-        str(code): 0 for code in [
-            200, 301, 400, 401, 403, 404, 405, 500]}
+    log["code_frequency"] = {str(code): 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]}
+
+    # Register the signal handler
+    signal.signal(signal.SIGINT, signal_handler)
 
     try:
         for line in sys.stdin:
             line = line.strip()
             match = regex.fullmatch(line)
-            if (match):
+            if match:
                 line_count += 1
                 code = match.group(1)
                 file_size = int(match.group(2))
@@ -38,11 +48,15 @@ if __name__ == "__main__":
                 # File size
                 log["file_size"] += file_size
 
-                # status code
-                if (code.isdecimal()):
+                # Status code
+                if code in log["code_frequency"]:
                     log["code_frequency"][code] += 1
 
-                if (line_count % 10 == 0):
+                if line_count % 10 == 0:
                     output(log)
+    except Exception as e:
+        # Handle unexpected errors gracefully
+        print(f"Error: {e}", file=sys.stderr)
     finally:
         output(log)
+
