d1c850e1ff70dcb64b9711ba71bb291b16bf6049
Mariam <kelanimaryam1234@gmail.com>
2024-08-06 07:22:00 -0700

    0-starwars_characters.js file for fetching film api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+  
+  if (response.statusCode !== 200) {
+    console.error('Failed to retrieve data');
+    return;
+  }
+  
+  const filmData = JSON.parse(body);
+  const characters = filmData.characters;
+
+  characters.forEach(characterUrl => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error('Error:', error);
+        return;
+      }
+      
+      if (response.statusCode !== 200) {
+        console.error('Failed to retrieve character data');
+        return;
+      }
+      
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+    });
+  });
+});
+
